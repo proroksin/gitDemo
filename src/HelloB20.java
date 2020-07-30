a01ba4d7daa5ea1072c363f1d3dd71e284b31152
proroksin <sinaverkemalov@gmail.com>
2020-07-29 20:54:12 -0500

    first comit

---
+++ src/HelloB20.java
@@ -0,0 +1,7 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+        System.out.println("Hello B20 it is my pleasure to be with you!");
+
+        // added  a line
+    }
+}
